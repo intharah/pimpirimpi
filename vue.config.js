ed30c663f1953d827fc4f644ef1d30c692925735
intharah <intharah@gmail.com>
2021-09-30 14:02:16 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     devServer: {
         open: process.platform === 'darwin',
         host: '0.0.0.0',
-        port: 8080, // CHANGE YOUR PORT HERE!
-        https: true,
+        //port: 8080, // CHANGE YOUR PORT HERE!
+        https: false,
         hotOnly: false,
     },
 }
