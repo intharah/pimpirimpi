c8401e3b109016c60861d5c24464115cdaac67c8
intharah <intharah@gmail.com>
2021-10-04 09:33:40 +0200

    update project

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         open: process.platform === 'darwin',
         host: '0.0.0.0',
         port: 8080, // CHANGE YOUR PORT HERE! DISABLE FOR HEROKU
-        https: true,
+        https: false,
         hotOnly: false,
         disableHostCheck: true // Option used to fix Invalid host header for deployment in Heroku
     },
